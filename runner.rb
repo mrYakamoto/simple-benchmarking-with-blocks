7abee0dbf92e0bc275e7710ff893bf1236b6e02a
Torey Hickman <torey@toreyhickman.com>
2015-08-17 13:09:20 -0500

    Remove comment from runner

--- runner.rb
+++ runner.rb
@@ -1,7 +1,5 @@
 require_relative 'benchmark'
 
-# Be careful, pasting this into IRB will take a long time to print.
-# It's a loooong string. :)
 
 long_string  = "apple"*100000000
 running_time = benchmark { long_string.reverse }
