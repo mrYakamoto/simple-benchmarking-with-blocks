d0127bda96da8950112fd84b135430e758e4603b
mrYakamoto <wehmenstein@gmail.com>
2015-12-01 18:48:59 -0800

    finalize simple benchmarking with blocks

--- benchmark.rb
+++ benchmark.rb
@@ -1,4 +1,8 @@
 # Accepts a block and returns how many seconds
 # it takes to execute the code in the block.
 def benchmark
+  start_time = Time.now
+  result = yield
+  end_time = Time.now
+  benchmark = end_time - start_time
 end
